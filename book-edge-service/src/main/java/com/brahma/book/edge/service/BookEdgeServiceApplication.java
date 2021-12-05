d14e433f8d546cb26f6e76cdec0d9b5e0084bd60
Balarka Brahma <balarka.cse05@gmail.com>
2021-12-05 19:21:50 +0530

    initial commit

---
+++ book-edge-service/src/main/java/com/brahma/book/edge/service/BookEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brahma.book.edge.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEdgeServiceApplication.class, args);
+	}
+
+}
