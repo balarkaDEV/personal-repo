7644ba1f1c41c3ae8f06be481339227f80b1df58
Balarka Brahma <balarka.cse05@gmail.com>
2021-12-05 20:17:14 +0530

    adding orderservice

--- book-details-service/src/main/java/com/brahma/bookmaster/service/BookDetailsServiceApplication.java
+++ book-details-service/src/main/java/com/brahma/bookmaster/service/BookDetailsServiceApplication.java
@@ -2,9 +2,7 @@ package com.brahma.bookmaster.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.brahma.bookmaster.service.repository")
 @SpringBootApplication
 public class BookDetailsServiceApplication {
 	public static void main(String[] args) {
